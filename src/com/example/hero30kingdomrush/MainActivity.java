583204002c39ef5b377db4abea71f3f62d5e4120
liujm1016 <liujm1016@thundersoft.com>
2012-11-13 17:23:05 +0800

    load texturepacker resource ok

--- src/com/example/hero30kingdomrush/MainActivity.java
+++ src/com/example/hero30kingdomrush/MainActivity.java
@@ -4,8 +4,33 @@ package com.example.hero30kingdomrush;
 
 
   
-import android.graphics.Color;  
-import android.opengl.GLES20;  
+import android.graphics.Color;
+import android.opengl.GLES20;
+
+import org.andengine.engine.camera.Camera;
+import org.andengine.engine.options.EngineOptions;
+import org.andengine.engine.options.ScreenOrientation;
+import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.andengine.entity.Entity;
+import org.andengine.entity.scene.Scene;
+import org.andengine.entity.scene.background.Background;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
+import org.andengine.extension.texturepacker.opengl.texture.util.texturepacker.TexturePack;
+import org.andengine.extension.texturepacker.opengl.texture.util.texturepacker.TexturePackLoader;
+import org.andengine.extension.texturepacker.opengl.texture.util.texturepacker.TexturePackTextureRegionLibrary;
+import org.andengine.extension.texturepacker.opengl.texture.util.texturepacker.TexturePackerTextureRegion;
+import org.andengine.extension.texturepacker.opengl.texture.util.texturepacker.exception.TexturePackParseException;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.ui.activity.SimpleBaseGameActivity;
+import org.andengine.util.debug.Debug;
   
 public class MainActivity extends SimpleBaseGameActivity {  
     public static final int CAMERA_WIDTH = 800;  
@@ -59,17 +84,56 @@ public class MainActivity extends SimpleBaseGameActivity {
         }  
   
         /* No background color needed as we have a fullscreen background sprite. */  
-        this.mScene.setBackgroundEnabled(false);  
-        this.mScene.getChildByIndex(LAYER_BACKGROUND).attachChild(  
-                new Sprite(0, 0, this.mBackgroundTextureRegion, this  
-                        .getVertexBufferObjectManager()));  
+    //    this.mScene.setBackgroundEnabled(false);  
+        mScene.setBackground(new Background(1, 1, 1));
+      //  this.mScene.getChildByIndex(LAYER_BACKGROUND).attachChild(  
+      //          new Sprite(0, 0, this.mBackgroundTextureRegion, this  
+       //                 .getVertexBufferObjectManager()));  
           
         this.mHelloText=new Text(CAMERA_WIDTH/2 - 80, CAMERA_HEIGHT/2 - 16, this.mFont, "Hello World !", this.getVertexBufferObjectManager());  
         this.mHelloText.setBlendFunction(GLES20.GL_SRC_ALPHA, GLES20.GL_ONE_MINUS_SRC_ALPHA);  
         this.mHelloText.setAlpha(0.6f);  
         this.mScene.getChildByIndex(LAYER_TEXT).attachChild(this.mHelloText);  
+        
+        TiledTextureRegion tiledTextureRegion = getTiledTextureFromPack("fireman");
+         
+         
+         AnimatedSprite sprite = new AnimatedSprite((CAMERA_WIDTH - tiledTextureRegion.getWidth()) / 2,
+                 (CAMERA_HEIGHT - tiledTextureRegion.getHeight()) / 2,
+                 tiledTextureRegion, this.getVertexBufferObjectManager());
+         
+         sprite.animate(100);
+        // sprite.
+         
+         this.mScene.attachChild(sprite);
+        
           
         return this.mScene;  
-    }  
+    } 
+    
+    private TiledTextureRegion getTiledTextureFromPack(String name) {
+        TexturePackTextureRegionLibrary packer =null;
+        TexturePack spritesheetTexturePack = null;
+        try {
+            spritesheetTexturePack = new TexturePackLoader(this.getTextureManager(), "gfx/")
+                    .loadFromAsset(getAssets(), name + ".xml");
+            spritesheetTexturePack.loadTexture();
+            packer = spritesheetTexturePack.getTexturePackTextureRegionLibrary();
+        } catch (final TexturePackParseException e) {
+            Debug.e(e);
+        }
+
+        TexturePackerTextureRegion[] obj = new TexturePackerTextureRegion[packer
+                .getIDMapping().size()];
+
+        for (int i = 0; i < packer.getIDMapping().size(); i++) {
+            obj[i] = packer.get(i);
+        }
+
+        TiledTextureRegion texture = new TiledTextureRegion(spritesheetTexturePack.getTexture(),
+                obj);
+
+        return texture;
+    }
       
 }  
